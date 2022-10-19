f111d437f18e3910edb68c8d53f19a2b572710df
sambit420 <sambit2024@gmail.com>
2022-10-19 22:30:56 +0530

    Added : Use Case 6 Problem - Calculating wages for month of an Employee

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -7,46 +7,58 @@ public class EmployeeWageComputation {
     public EmployeeWageComputation() {
         System.out.println("Welcome to Employee Wage Computation!");
     }
+        public static void main(String[] args) {
+            //Declaring the variables.
+            Random random = new Random();
+            int randomCheck;
+            int empHour = 0;
+            int wagePerHour = 20;
+            int salary = 0;
+            int numWorkingDays = 20;
+            int monthlySalary = 0;
+            int maxHours = 100;
+            int totalWorkingHours = 0;
+            int totalWorkingDays = 0;
 
-    public static void main(String[] args) {
-        //Declaring the variables.
-        Random random = new Random();
-        int randomCheck;
-        int empHour;
-        int wagePerHour = 20;
-        int salary = 0;
-        int numWorkingDays = 20;
-        int monthlySalary = 0;
-
-        //Calculating the number of working days of an Employee randomly.
-        for (int i = 1; i <= numWorkingDays; i++) {
-            randomCheck = random.nextInt(3);
-            switch (randomCheck) {
-                case 0:
-                    System.out.println("Employee is absent");
-                    empHour = 0;
-                    salary = empHour * wagePerHour;
-                    break;
-                case 1:
-                    System.out.println("Employee is working part-time");
-                    int partTimeHour = 8;
-                    empHour = partTimeHour;
-                    salary = empHour * wagePerHour;
-                    break;
-                case 2:
-                    System.out.println("Employee is working full-time");
-                    int fullTimeHour = 16;
-                    empHour = fullTimeHour;
-                    salary = empHour * wagePerHour;
-                    break;
+        /*Calculating the number of working days of an Employee randomly unless it satisfies the condition
+          of total working hours or days is reached for a month.*/
+            while (totalWorkingDays < numWorkingDays && totalWorkingHours < maxHours)
+            {
+                totalWorkingDays++;
+                randomCheck = random.nextInt(3);
+                switch (randomCheck) {
+                    case 0 :
+                        System.out.println("Employee is absent");
+                        empHour=0;
+                        salary = empHour * wagePerHour;
+                        break;
+                    case 1 :
+                        System.out.println("Employee is working part-time");
+                        int partTimeHour = 8;
+                        empHour = partTimeHour;
+                        salary = empHour * wagePerHour;
+                        break;
+                    case 2 :
+                        System.out.println("Employee is working full-time");
+                        int fullTimeHour = 16;
+                        empHour = fullTimeHour;
+                        salary = empHour * wagePerHour;
+                        break;
+                }
+                monthlySalary += salary;
+                totalWorkingHours += empHour;
             }
-            monthlySalary = monthlySalary + salary;
+            System.out.println();
+            System.out.println("Total working days of an Employee is: "+totalWorkingDays+"\n");
+            System.out.println("Total working hour of an Employee is: "+totalWorkingHours+"\n");
+            System.out.println("Salary of an Employee in a month is: $"+monthlySalary);
         }
-        System.out.println("Salary of an Employee in a month is: $" + monthlySalary);
+        }
 
 
-    }
-}
+
+
+
 
 
 
