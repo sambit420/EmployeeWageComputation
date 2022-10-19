36a782723a65c4a9d8d2be42313292baee1b0227
sambit420 <sambit2024@gmail.com>
2022-10-19 19:44:14 +0530

    Added : Use Case 2 Problem - Calucalte daily Employee wage

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -8,16 +8,23 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         //initialize the constructor by creating a object of a class
         EmployeeWageComputation employee =new EmployeeWageComputation();
-        //Declaring the varibles.
+        //Declaring the variables.
         double isPresent = 1;
         double randomCheck = (double) Math.floor((Math.random() * 10) % 2);
+        int fullDayHour = 8;
+        int wagePerHour = 20;
+        int salary = 0;
 
-        //Checking if the Employee is present!!!
+        ////Checking if the Employee is present, then calculating his/her daily wage.
         if (randomCheck == isPresent) {
             System.out.println("Employee is present");
+            salary = fullDayHour * wagePerHour;
+            System.out.println("Salary is: $" + salary);
         } else {
-             System.out.println("Employee is absent");
+            System.out.println("Employee is absent");
+            System.out.println("Salary is: $" + salary);
         }
+
 
     }
 }
