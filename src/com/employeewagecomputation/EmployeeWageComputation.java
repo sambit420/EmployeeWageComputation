c40defa59437eafad0c79bc3665d066631748a9f
sambit420 <sambit2024@gmail.com>
2022-10-24 05:29:59 +0530

    Added : Use Case 8 Problem - Computing wages for a month of an Employee

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -4,52 +4,43 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
         //Declaring the constants
-        private final int isPartTime = 1;
-        private final int isFullTime = 2;
-        private final int wagePerHour = 20;
-        private final int numWorkingDays = 20;
-        private final int maxHours = 100;
+        private final static int isPartTime = 1;
+        private final static int isFullTime = 2;
 
         // Default Constructor
         public EmployeeWageComputation() {
                 System.out.println("Welcome to Employee Wage Computation Program..!");
         }
 
-        @Override
-        public String toString() {
-                return "EmployeeWageComputation{" +
-                        "isPartTime=" + isPartTime +
-                        ", isFullTime=" + isFullTime +
-                        ", wagePerHour=" + wagePerHour +
-                        ", numWorkingDays=" + numWorkingDays +
-                        ", maxHours=" + maxHours +
-                        '}';
-        }
-
         public static void main(String[] args) {
 
                 //Initializing the constructor by creating the object of the class.
                 EmployeeWageComputation employee = new EmployeeWageComputation();
-                System.out.println("Constants Taken for the computation program are below :\n"+employee);
-                System.out.println();
-                System.out.println("Calculating the salary : ............");
-                printData(calculateSalary(employee)); //Parsing value from calculateSalary Method.
 
+                //Calculating employee wage for different companies.
+                System.out.println("Calculating the salary for different companies : ............");
+                printData(calculateSalary("Google",40,20,100)); //Parsing value from calculateSalary Method.
+                printData(calculateSalary("Microsoft",20,18,150));
+                printData(calculateSalary("Amazon",60,22,180));
+                printData(calculateSalary("Apple",30,26,200));
         }
 
         /*Calculating the number of working days of an Employee randomly unless it satisfies the condition
               of total working hours or days is reached for a month.*/
-        public static int[] calculateSalary(EmployeeWageComputation employee) {
+        public static int[] calculateSalary(String s, int wagePerHour, int numWorkingDays, int maxHours) {
 
                 //Declaring the variables
                 int randomCheck;
-                int empHour;
-                int salary;
+                int empHour = 0;
+                int salary = 0;
                 int monthlySalary = 0;
                 int totalWorkingHours = 0;
                 int totalWorkingDays = 0;
 
-                while (totalWorkingDays < employee.numWorkingDays && totalWorkingHours < employee.maxHours)
+                System.out.println();
+                System.out.println("For Company : "+s+"...................");
+                System.out.println();
+                while (totalWorkingDays < numWorkingDays && totalWorkingHours < maxHours)
                 {
                         totalWorkingDays++;
                         randomCheck = (int) Math.floor((Math.random() * 10) % 3);
@@ -57,22 +48,22 @@ public class EmployeeWageComputation {
                         {
                                 System.out.println("Employee is absent");
                                 empHour = 0;
-                                salary = empHour * employee.wagePerHour;
+                                salary = empHour * wagePerHour;
                         }
-                        else if (randomCheck == employee.isPartTime)
+                        else if (randomCheck == isPartTime)
                         {
 
                                 System.out.println("Employee is working part-time");
                                 int partTimeHour = 4;
                                 empHour = partTimeHour;
-                                salary = empHour * employee.wagePerHour;
+                                salary = empHour * wagePerHour;
                         }
-                        else
+                        else if (randomCheck == isFullTime)
                         {
                                 System.out.println("Employee is working full-time");
                                 int fullTimeHour = 8;
                                 empHour = fullTimeHour;
-                                salary = empHour * employee.wagePerHour;
+                                salary = empHour * wagePerHour;
                         }
                         monthlySalary += salary;
                         totalWorkingHours += empHour;
@@ -90,6 +81,7 @@ public class EmployeeWageComputation {
                 System.out.println("Total working days of an Employee is : "+totalWorkingDays+"\n");
                 System.out.println("Total working hour of an Employee is : "+totalWorkingHours+"\n");
                 System.out.println("Salary of an Employee in a month is : $"+monthlySalary);
+                System.out.println();
         }
 
         }
